8413da6e232c2bc8c214fef3c899e51d2f8073bf
Pandji Setiya Budhi Artha <pandjisetiya@gmail.com>
2022-09-08 21:04:41 +0800

    edit about us

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './component/Home/Home';
 import Destination from './component/Destination/Destination';
