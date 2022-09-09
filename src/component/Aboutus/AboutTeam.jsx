aee91fe431c80daab0351ffb0aa60f2a3384cbbf
Pandji Setiya Budhi Artha <pandjisetiya@gmail.com>
2022-09-09 17:29:43 +0800

    edit about us #2

---
+++ src/component/Aboutus/AboutTeam.jsx
@@ -0,0 +1,122 @@
+import React from 'react'
+import './StyleAbout.css'
+import { Link } from 'react-router-dom'
+import { BsTwitter } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+import { BsFacebook } from "react-icons/bs";
+
+function AboutTeam() {
+    return (
+        <>
+         <div className="container">
+        <h4>Ngiung Team Project</h4>
+                <div className="row row-cols-1 row-cols-lg-2 row-cols-md-2 row-cols-xl-3">
+                    <div className="col">
+                        <div className="card radius-15">
+                            <div className="card-body text-center">
+                                <div className="p-4 border radius-15">
+                                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" width="110" height="110" className="rounded-circle shadow" alt="" />
+                                        <h5 className="mb-0 mt-5">M. Haykal</h5>
+                                        <p className="mb-3">Webdeveloper</p>
+                                        <div className="list-inline contacts-social mt-3 mb-3"> <Link to="" href="javascript:;" className="list-inline-item bg-facebook text-white border-0"><BsFacebook/></Link >
+                                            <Link to="" className="list-inline-item bg-twitter text-white border-0"> <BsTwitter/> </Link >
+                                            <Link to="" className="list-inline-item bg-linkedin text-white border-0"> <BsLinkedin/> </Link >
+                                        </div>
+                                        <div className="d-grid"> <Link to="" href="#" className="btn btn-outline-primary radius-15">Contact Me</Link >
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card radius-15">
+                            <div className="card-body text-center">
+                                <div className="p-4 border radius-15">
+                                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" width="110" height="110" className="rounded-circle shadow" alt="" />
+                                        <h5 className="mb-0 mt-5">Pandji Setiya B.A</h5>
+                                        <p className="mb-3">Webdeveloper</p>
+                                        <div className="list-inline contacts-social mt-3 mb-3"> <Link to="" href="javascript:;" className="list-inline-item bg-facebook text-white border-0"><BsFacebook/></Link >
+                                            <Link to="" className="list-inline-item bg-twitter text-white border-0"> <BsTwitter/> </Link >
+                                            <Link to="" className="list-inline-item bg-linkedin text-white border-0"> <BsLinkedin/> </Link >
+                                        </div>
+                                        <div className="d-grid"> <Link to="" href="#" className="btn btn-outline-primary radius-15">Contact Me</Link >
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card radius-15">
+                            <div className="card-body text-center">
+                                <div className="p-4 border radius-15">
+                                    <img src="https://bootdey.com/img/Content/avatar/avatar1.png" width="110" height="110" className="rounded-circle shadow" alt="" />
+                                        <h5 className="mb-0 mt-5">Achmad Syakban</h5>
+                                        <p className="mb-3">UI Developer</p>
+                                        <div className="list-inline contacts-social mt-3 mb-3"> <Link to="" href="javascript:;" className="list-inline-item bg-facebook text-white border-0"><BsFacebook/></Link >
+                                            <Link to="" className="list-inline-item bg-twitter text-white border-0"> <BsTwitter/> </Link >
+                                            <Link to="" className="list-inline-item bg-linkedin text-white border-0"> <BsLinkedin/> </Link >
+                                        </div>
+                                        <div className="d-grid"> <Link to="" href="#" className="btn btn-outline-primary radius-15">Contact Me</Link >
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card radius-15">
+                            <div className="card-body text-center">
+                                <div className="p-4 border radius-15">
+                                    <img src="https://bootdey.com/img/Content/avatar/avatar6.png" width="110" height="110" className="rounded-circle shadow" alt="" />
+                                        <h5 className="mb-0 mt-5">Linda Prikitiew</h5>
+                                        <p className="mb-3">Graphic Designer</p>
+                                        <div className="list-inline contacts-social mt-3 mb-3"> <Link to="" href="javascript:;" className="list-inline-item bg-facebook text-white border-0"><BsFacebook/></Link >
+                                            <Link to="" className="list-inline-item bg-twitter text-white border-0"> <BsTwitter/> </Link >
+                                            <Link to="" className="list-inline-item bg-linkedin text-white border-0"> <BsLinkedin/> </Link >
+                                        </div>
+                                        <div className="d-grid"> <Link to="" href="#" className="btn btn-outline-primary radius-15">Contact Me</Link >
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card radius-15">
+                            <div className="card-body text-center">
+                                <div className="p-4 border radius-15">
+                                    <img src="https://bootdey.com/img/Content/avatar/avatar2.png" width="110" height="110" className="rounded-circle shadow" alt="" />
+                                        <h5 className="mb-0 mt-5">Raya Haleluyah</h5>
+                                        <p className="mb-3">Android Developer</p>
+                                        <div className="list-inline contacts-social mt-3 mb-3"> <Link to="" href="javascript:;" className="list-inline-item bg-facebook text-white border-0"><BsFacebook/></Link >
+                                            <Link to="" className="list-inline-item bg-twitter text-white border-0"> <BsTwitter/> </Link >
+                                            <Link to="" className="list-inline-item bg-linkedin text-white border-0"> <BsLinkedin/> </Link >
+                                        </div>
+                                        <div className="d-grid"> <Link to="" href="#" className="btn btn-outline-primary radius-15">Contact Me</Link >
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card radius-15">
+                            <div className="card-body text-center">
+                                <div className="p-4 border radius-15">
+                                    <img src="https://bootdey.com/img/Content/avatar/avatar2.png" width="110" height="110" className="rounded-circle shadow" alt="" />
+                                        <h5 className="mb-0 mt-5">Nisa Gaib</h5>
+                                        <p className="mb-3">Android Developer</p>
+                                        <div className="list-inline contacts-social mt-3 mb-3"> <Link to="" href="javascript:;" className="list-inline-item bg-facebook text-white border-0"><BsFacebook/></Link >
+                                            <Link to="" className="list-inline-item bg-twitter text-white border-0"> <BsTwitter/> </Link >
+                                            <Link to="" className="list-inline-item bg-linkedin text-white border-0"> <BsLinkedin/> </Link >
+                                        </div>
+                                        <div className="d-grid"> <Link to="" href="#" className="btn btn-outline-primary radius-15">Contact Me</Link >
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default AboutTeam
--- src/component/Aboutus/AboutUs.jsx
+++ src/component/Aboutus/AboutUs.jsx
@@ -4,29 +4,31 @@ import './StyleAbout.css'
 import Carousel from 'react-bootstrap/Carousel';
 import Card from 'react-bootstrap/Card';
 
+
 // import gambar1 from '../../assets/background/bg1.jpg';
 import gambar2 from '../../assets/background/bg2.jpg';
 import gambar3 from '../../assets/background/bg4.jpg';
+import AboutTeam from './AboutTeam';
 
 function AboutUs() {
   return (
     <>
       <NavBar />
-    <div className="container">
-      <div className="row">
-      <Carousel variant="dark">
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={gambar3}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-        <h1 className='title hover-underline-animation'>Welcome to <span>Ngiung Travel Tour</span></h1>
-        <h5 className="subtitle">Destinasi Wisata dan Tour Sumbawa Nusa Tenggara Barat</h5>
-        </Carousel.Caption>
-      </Carousel.Item>
-      {/* <Carousel.Item>
+      <div className="container">
+        <div className="row">
+          <Carousel variant="dark">
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={gambar3}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h1 className='title hover-underline-animation'>Welcome to <span>Ngiung Travel Tour</span></h1>
+                <h5 className="subtitle">Destinasi Wisata dan Tour Sumbawa Nusa Tenggara Barat</h5>
+              </Carousel.Caption>
+            </Carousel.Item>
+            {/* <Carousel.Item>
         <img
           className="d-block w-100"
           src={gambar2}
@@ -50,18 +52,20 @@ function AboutUs() {
           </p>
         </Carousel.Caption>
       </Carousel.Item> */}
-    </Carousel>
-    <div className="logo col-lg-4 col-md-5 col-sm-1 py-2 mt-3">
-    <Card style={{ width: '18rem'}}>
-      <Card.Img variant="top" src={gambar2} />
-    </Card>
-    </div>
-    <div className="col-lg-8 col-md-7 py-2">
-      <h1>About Us</h1>
-      <p className='subAbout'> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-    </div>
-    </div>
-    </div>
+          </Carousel>
+          <div className="logo col-lg-4 col-md-5 col-sm-1 py-2 mt-3">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={gambar2} />
+            </Card>
+          </div>
+          <div className="col-lg-8 col-md-7 py-2">
+            <h1>About Us</h1>
+            <p className='subAbout'> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </div>
+        </div>
+      </div>
+
+      <AboutTeam />
     </>
   )
 }
