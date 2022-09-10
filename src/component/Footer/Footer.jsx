995ccd9593767ba80d1172da74416b99504fe08d
lindlinda52 <lindarahayuningsih9@gmail.com>
2022-09-10 09:53:16 +0800

    page footer

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
+import {FaFacebook,  FaTwitter,  FaWhatsapp} from 'react-icons/fa'
+
 
 function Footer() {
     return (
-        <section className="footer">
-            <ul>
-                WhatsApp
-            </ul>
-            <ul>
-                E-mail
-            </ul>
-            <ul>
-                Sumbawa NTB
-            </ul>
-        </section>
-    );
+        <div className='footer'>
+            <div className="container">
+                <div className="top">
+                    <h3>Ngiung TRVL</h3>
+                    <div className="social">
+                        <FaFacebook className='icon' />
+                        <FaTwitter className='icon' />
+                        <FaWhatsapp className='icon' />
+                    </div>
+                </div>
+                <div className="bottom">
+                    <div className="left">
+                        <ul>
+                            Facebook
+                        </ul>
+                    </div>
+                    <div className="right">
+                        <ul>
+                            Twitter
+                        </ul>
+                    </div>
+                    <div className="right">
+                        <ul>
+                            WhatsApp
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
-export default Footer
 
+export default Footer
