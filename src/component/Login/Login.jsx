509b4d6177bc225bae6e1beff53d757a50d8dbb2
Qayla Rayani Ibensani <rayaibensani@gmail.com>
2022-09-11 19:47:56 +0800

    Edit Login dan Register

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -7,25 +7,25 @@ function Login() {
    
     return (
      <>
-    {/* //    <div classNameName="maincontainer"> */}
-        <div className="container-fluid">
-            <div className="row no-gutter">
+      <div className="maincontainer">
+        <div className='background-img'></div>
+        <div id='Body' className="container">
+            <div className="row px-5">
                
                 <div className="col-md-6 d-none d-md-flex bg-image">
-                    {/* <h1>Ngiung TRVL</h1> */}
                 </div>
                 
                 <div className="col-md-6 bg-light">
                     <div className="login d-flex align-items-center py-5">
                        
-                        <div className="container">
+                        <div className="container-fluid">
                             <div className="row">
                                 <div className="col-lg-10 col-xl-7 mx-auto">
-                                    <h3 className="text-center display-4">Login page!</h3>
-                                    <p className="text-center text-muted mb-4">Welcome Back to our page!, please fill the login forms first before stated</p>
+                                    <h3 className="text-center display-4">Login!</h3>
+                                    <p className="text-center text-muted mb-4">Welcome Back to our page!, please fill out the login form before starting</p>
                                     <form>
                                         <div className="mb-3">
-                                            <input id="inputEmail" type="email" placeholder="Email address" required="" autoFocus="" className="form-control rounded-pill border-0 shadow-sm px-4" />
+                                            <input id="inputEmail" type="email" placeholder="Email" required="" autoFocus="" className="form-control rounded-pill border-0 shadow-sm px-4" />
                                         </div>
                                         <div className="mb-3">
                                             <input id="inputPassword" type="password" placeholder="Password" required="" className="form-control rounded-pill border-0 shadow-sm px-4 text-primary" />
@@ -34,7 +34,7 @@ function Login() {
                                         <button type="submit" className="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm">Sign in</button>
                                         </div>
                                         
-                                        <div className="text-center d-flex justify-content-between mt-4"><p>Don't have an account, let's register first <Link to="/register" className="font-italic text-muted"> 
+                                        <div className="text-center d-flex justify-content-between mt-4"><p>Don't have an account?, let's register first <Link to="/register" className="font-italic text-muted"> 
                                                 <u>Register</u></Link></p></div>
                                         <div className="text-center"><p>Back To Home <Link to="/" className="font-italic text-muted"> 
                                         <u>Home</u></Link></p></div>
@@ -46,7 +46,7 @@ function Login() {
                 </div>
             </div>
         </div>
-        {/* </div> */}
+        </div>
         </>
 )
 }
