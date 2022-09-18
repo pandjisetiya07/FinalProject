7174d9d18783df02ecb1f263884db37f163778ef
Pandji Setiya Budhi Artha <pandjisetiya@gmail.com>
2022-09-18 23:57:10 +0800

    edit transaksi & login register

--- src/component/Navbar/NavBar.jsx
+++ src/component/Navbar/NavBar.jsx
@@ -7,8 +7,24 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import {CheckLogin} from "../../helpers/checkLogin" 
+import {useNavigate} from "react-router-dom"
+import Swal from "sweetalert2";
 
 function NavBar() {
+  const navigate = useNavigate()
+  const {isLogin} = CheckLogin()
+  const handleLogout = () => {
+    localStorage.removeItem('login')
+    Swal.fire({
+      title: "Berhasil Log Out ",
+      text: "Silahkan login kembali..",
+      icon: "success",
+      button: `/Login`,
+    })
+    navigate('/login')
+  }
+
   return (
     <>
       <Navbar className='navbarr navbar-dark' expand="lg">
@@ -31,12 +47,14 @@ function NavBar() {
               <Nav.Link href="/aboutus">
                 About
               </Nav.Link>
-              <Nav.Link href="/transaksi">
-                Transaksi
-              </Nav.Link>
-              <Nav.Link href="/Login">
+              {
+                isLogin ?
+                  <button onClick={handleLogout}>Log Out</button>
+                :
+                  <Nav.Link href="/Login">
                 Login
               </Nav.Link>
+              }
             </Nav>
             <Form className="d-flex">
               <Form.Control
