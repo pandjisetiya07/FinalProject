9913522b6eb9be4017931380962c2f7962873a4d
Ahmad Syakban <ahmadsyakban240@gmail.com>
2022-09-21 15:49:58 +0700

    Revisi

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
         <NavBar />
         <div className='container'>
             <div className="titleHead">
-                <h1 className='title hover-underline-animation'>{context.lang==='en' ? 'Welcome to' : 'Selamat Datang'} <span>{context.lang==='en' ? 'NgiungTravel' : 'di NgiungTravel'}</span></h1>
+                <h1 className='title hover-underline-animation'>{context.lang==='en' ? 'Welcome to' : 'Selamat Datang'} <span>{context.lang==='en' ? 'NgiungTRVL' : 'di NgiungTRVL'}</span></h1>
             </div>
             <div className="subHead">
                 <h5 className="subtitle">
